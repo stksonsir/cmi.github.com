0dc8e98f0fb0b53d1343dca9562fedf112f1747a
stksonsir <st.k.son.sir@gmail.com>
2026-03-04 15:20:50 +0800

    Deploy Centurion Ministries International website

--- App.jsx
+++ App.jsx
@@ -1,12 +1,15 @@
+// Centurion Ministries component
+
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Centurion Ministries Website</h1>
-      <p>Welcome to the Centurion Ministries website!</p>
-    </div>
-  );
+const CenturionMinistries = () => {
+    return (
+        <div>
+            <h1>Welcome to Centurion Ministries International</h1>
+            <p>Your hub for resources and information.</p>
+            {/* Additional component content goes here */}
+        </div>
+    );
 };
 
-export default App;
+export default CenturionMinistries;
